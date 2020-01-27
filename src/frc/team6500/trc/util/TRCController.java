d1809a3dc88ecf4dd86b7bb328add0957d29f253
rightshiftoverflow <cocoanut99@gmail.com>
2020-01-27 12:02:35 -0500

    Implement abstract methods

--- src/frc/team6500/trc/util/TRCController.java
+++ src/frc/team6500/trc/util/TRCController.java
@@ -1,7 +1,6 @@
 package frc.team6500.trc.util;
 
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.GenericHID;
 
 /**
@@ -10,6 +9,7 @@ import edu.wpi.first.wpilibj.GenericHID;
  */
 public class TRCController extends GenericHID
 {
+	public GenericHID.HIDType type;
 	/**
 	 *	Create a new TRCController of a specified type
 	 *	@param port the port number on which the controller is plugged in
@@ -39,5 +39,31 @@ public class TRCController extends GenericHID
 	public double getAxis(TRCAnalogHI ahi)
 	{
 		return this.getRawAxis(ahi.getAxis(this.type));
+	}
+
+	public double getX(GenericHID.Hand hand)
+	{
+		if (this.type == GenericHID.HIDType.kHIDGamepad) // is xbox
+		{
+			switch (hand)
+			{
+				case kLeft:	return getRawAxis(XboxController.Axis.kRightX.value);
+				case kRight: return getRawAxis(XboxController.Axis.kRightX.value);
+			}
+		}
+		return this.getX();
+	}
+
+	public double getY(GenericHID.Hand hand)
+	{
+		if (this.type == GenericHID.HIDType.kHIDGamepad) // is xbox
+		{
+			switch (hand)
+			{
+				case kLeft:	return getRawAxis(XboxController.Axis.kRightY.value);
+				case kRight: return getRawAxis(XboxController.Axis.kRightY.value);
+			}
+		}
+		return this.getX();
 	}
 }
