04b24d997c3c294168945dfb17e5d6b76856fc38
rightshiftoverflow <cocoanut99@gmail.com>
2020-02-11 20:15:29 -0500

    Add support for inversion

--- src/frc/team6500/trc/sensor/TRCEncoderGroup.java
+++ src/frc/team6500/trc/sensor/TRCEncoderGroup.java
@@ -3,6 +3,7 @@ package frc.team6500.trc.sensor;
 public class TRCEncoderGroup implements TRCEncoder
 {
 	private TRCEncoder[] encoders;
+	private double inversionFactor = 1.0;
 
 	/**
 	 *	Create a group of encoders
@@ -31,27 +32,40 @@ public class TRCEncoderGroup implements TRCEncoder
 	
 	/**
 	 * Gives the average distance all encoders have traveled
-	 * 
 	 * @return The average of the distance traveled by all the encoders
 	 */
 	public double getDistance()
 	{
 		double distancesum = 0.0;
 		for (TRCEncoder e : encoders) distancesum += e.getDistance();
-		return distancesum / encoders.length;
+		return (distancesum / encoders.length) * inversionFactor;
 	}
 
 	/**
-	 * Gives the average absolute (absolute value) distance all encoders have traveled; useful for measuring rotation
-	 * 
+	 * Gives the average absolute (absolute value) distance all encoders have traveled; useful for measuring rotation.
 	 * @return The average of the absolute distances traveled by all the encoders
 	 */
 	public double getAbsoluteDistance()
 	{
 		double distancesum = 0.0;
 		for (TRCEncoder e : encoders) distancesum += Math.abs(e.getDistance());
-		return distancesum / encoders.length;
+		return (distancesum / encoders.length) * inversionFactor;
 	}
+
+	/**
+	 * Set if the encoder's value should be inverted
+	 * @param inverted the inversion of the group
+	 */
+	public void setInverted(boolean inverted)
+	{
+		if (inverted) inversionFactor = -1.0;
+		else inversionFactor = 1.0;
+	}
+
+	/**
+	 * @return if the group is inverted
+	 */
+	public boolean getInverted() { return (this.inversionFactor == -1.0); }
 	
 	/**
 	 * Use this method after driving in a straight line to calibrate them to account for mechanical irregularities
